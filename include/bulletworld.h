b22c553347d81fec30acd7ec1ef91f1622bea538
thibaud-fdb <thibaud.fdb@gmail.com>
2016-02-08 14:59:55 +0300

    added a bullet example class

---
+++ include/bulletworld.h
@@ -0,0 +1,11 @@
+#ifndef BULLETWORLD_H
+#define BULLETWORLD_H
+
+
+class BulletWorld
+{
+public:
+    BulletWorld();
+};
+
+#endif // BULLETWORLD_H
---
+++ source/bulletworld.cpp
@@ -0,0 +1,6 @@
+#include "include/bulletworld.h"
+
+BulletWorld::BulletWorld()
+{
+
+}
